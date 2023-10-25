606754b000ccd1468fe10d398330e711a23bed9a
Dinis <scorpaust@hotmail.com>
2023-10-26 00:23:06 +0100

    Player Animations

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -23,8 +23,37 @@ public class PlayerController : MonoBehaviour
     [Tooltip("Determines the upward force applied when the character jumps.")]
     [SerializeField] private float jumpForce;
 
-    // Start is called before the first frame update
-    void Start()
+    [Tooltip("Indicates whether the player has the ability to perform a second jump while airborne.")]
+    private bool canDoubleJump;
+
+    [Header("State")]
+
+    [Tooltip("Indicates whether the player is currently in contact with the ground.")]
+    private bool isGrounded;
+
+	[Tooltip("Reference point from which to check if the player is in contact with the ground.")]
+	[SerializeField] private Transform groundCheckPoint;
+
+	[Tooltip("Radius around the groundCheckPoint within which the game checks for ground.")]
+	[SerializeField] private float groundCheckRadius;
+
+	[Tooltip("Radius around the leftPlatformsCheckPoint or rightPlatformCheckpoints within which the game checks for platforms.")]
+	[SerializeField] private float platformCheckRadius;
+
+	[Tooltip("Reference point from which to check if the player is in contact with a left platform.")]
+	[SerializeField] private Transform leftPlatformsCheckPoint;
+
+	[Tooltip("Reference point from which to check if the player is in contact with a right platform.")]
+	[SerializeField] private Transform rightPlatformsCheckPoint;
+
+	[Tooltip("Layer(s) considered as 'ground'. Used to detect if the player is on the ground.")]
+	[SerializeField] private LayerMask whatIsGround;
+
+	[Tooltip("Layer(s) considered as 'plaatform'. Used to detect if the player is on the side of some platform to double jump.")]
+	[SerializeField] private LayerMask whatIsPlatform;
+
+	// Start is called before the first frame update
+	void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
@@ -32,13 +61,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Move();
+		CheckGrounded();
 
-        HandleJumping();
-    }
+		Move();
+		
+		HandleJumping();
+	}
+
+    void Update()
+    {
+		HandleInputs();
+	}
 
     void Move()
     {
+		// Sets the active speed based on whether the Left Shift key is pressed: 'runSpeed' if pressed, otherwise 'moveSpeed'.
 		activeSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : moveSpeed;
 
 		// Set the horizontal velocity of the object based on player input, while preserving the current vertical velocity.
@@ -49,8 +86,49 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump"))
         {
-			// Set the vertical velocity of the object to the jump force, while preserving the current horizontal velocity.
-			rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
+            if (isGrounded == true)
+            {
+                Jump();
+                canDoubleJump = true;
+            }
+            else if (canDoubleJump)
+            {
+				if (CanDoubleJumpOffPlatform())
+				{
+					Jump();
+					canDoubleJump = false;
+				}
+				else
+				{
+					canDoubleJump = false;
+				}
+			}
+			else
+			{
+				canDoubleJump = false;
+			}
+		}
     }
+
+	void CheckGrounded()
+	{
+		isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+	}
+
+	void HandleInputs()
+	{
+		activeSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : moveSpeed;
+	}
+
+	void Jump()
+    {
+		// Set the vertical velocity of the object to the jump force, while preserving the current horizontal velocity.
+		rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+	}
+
+	bool CanDoubleJumpOffPlatform()
+	{
+		return Physics2D.OverlapCircle(leftPlatformsCheckPoint.position, platformCheckRadius, whatIsPlatform) ||
+			   Physics2D.OverlapCircle(rightPlatformsCheckPoint.position, platformCheckRadius, whatIsPlatform);
+	}
 }
